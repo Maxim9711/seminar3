f8a0f0edd54952754ba4e3c0adf0481cf5c3c239
Max <Bruce.97@mail.ru>
2023-04-22 08:10:39 +0300

    all files add

---
+++ task18.py
@@ -0,0 +1,23 @@
+#Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X.
+#Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
+#В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+
+
+from random import randrange
+
+N = int(input("N: "))
+A = [randrange(1000) for i in range(N)]
+print(A)
+
+X = int(input("X: "))
+
+
+min = abs(X - A[0])
+index = 0
+for i in range(1, N):
+    count = abs(X - A[i])
+    if count < min:
+        min = count
+        index = i
+
+print(f"close value {A[index]}")
